6ecd336e0547d4af64e2dfb6f8569a7bc4663da4
Aditya Kumar Singh <sadityakumar9211@gmail.com>
2023-07-24 11:07:52 +0530

    ADD: chain.go
    Signed-off-by: Aditya Kumar Singh <sadityakumar9211@gmail.com>

--- orderer/consensus/bdls/chain.go
+++ orderer/consensus/bdls/chain.go
@@ -1,1 +1,45 @@
+package bdls
 
+import (
+	"fmt"
+
+	bdls "github.com/BDLS-bft/bdls"
+)
+
+type Chain struct {
+	// Chain Struct
+	config *bdls.Config
+}
+
+func (c *Chain) Start() {
+	consensus, err := bdls.NewConsensus(c.config)
+	checkErrNil(err)
+	fmt.Println(consensus)
+
+}
+
+func (c *Chain) Order() {
+
+}
+
+func (c *Chain) Configure() {
+
+}
+
+func (c *Chain) WaitReady() {
+
+}
+
+func (c *Chain) Errored() {
+
+}
+
+func (c *Chain) Halt() {
+
+}
+
+func checkErrNil(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
