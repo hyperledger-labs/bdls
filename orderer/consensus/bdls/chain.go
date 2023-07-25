c029856ca08b191a6d1ec830373798fc6bec30c1
tonymathen <tonymathen@gmail.com>
2023-07-24 18:53:51 -0700

    add bdls package and chain.go
    Signed-off-by: tonymathen <tonymathen@gmail.com>

---
+++ orderer/consensus/bdls/chain.go
@@ -0,0 +1,26 @@
+package bdls
+
+import (
+	bdls "github.com/BDLS-bft/bdls"
+	cb "github.com/hyperledger/fabric-protos-go/common"
+)
+
+
+
+type Chain struct {
+	config *bdls.Config
+	channelID string
+}
+
+
+func (c *Chain) Order(env *cb.Envelope, configSeq uint64) error {
+	return nil
+}
+
+func (c *Chain) Configure(config *cb.Envelope, configSeq uint64) error {
+	return nil
+}
+
+func (c *Chain) Errored() <-chan struct{} {
+	return nil
+}
